a9cc0807c046c4742f0f1f466e076cc6ad20ccff
Mao <62439748+Maoshijin@users.noreply.github.com>
2026-01-19 11:11:03 +0000

    更新 foton.js

--- wenjian/foton.js
+++ wenjian/foton.js
@@ -1,3 +1,6 @@
 # 福田E家屏蔽更新弹窗
 ^https:\/\/czyl\.foton\.com\.cn\/ehomes-new\/homeManager\/api\/User\/getVersion url script-response-body 
 https://raw.githubusercontent.com/Maoshijin/wenjian/refs/heads/main/foton.js
+
+[MITM]
+hostname = czyl.foton.com.cn
