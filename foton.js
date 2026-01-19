8f5ac55caa8dc541c4edb90b42b9d1003ce5df3b
Mao <62439748+Maoshijin@users.noreply.github.com>
2026-01-19 11:06:53 +0000

    上传外部文本 2026/1/19 19:06:51

---
+++ foton.js
@@ -0,0 +1,3 @@
+# 福田E家屏蔽更新弹窗
+^https:\/\/czyl\.foton\.com\.cn\/ehomes-new\/homeManager\/api\/User\/getVersion url script-response-body 
+https://raw.githubusercontent.com/Maoshijin/wenjian/refs/heads/main/foton.js
