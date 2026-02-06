0a162588b8a2caba30c126f3ffc715cf840e6f5b
Mao <62439748+Maoshijin@users.noreply.github.com>
2026-02-06 16:33:35 +0800

    Update foton.js

--- wenjian/foton.js
+++ wenjian/foton.js
@@ -19,44 +19,37 @@ http-response ^https:\/\/czyl\.foton\.com\.cn\/ehomes-new\/homeManager\/(api\/Us
 hostname = czyl.foton.com.cn
 */
 
+
 var body = $response.body;
 var url = $request.url;
 
-// 递归遍历函数：不管藏在第几层，都要揪出来
-function deepClean(obj) {
+function nukeUpdate(obj) {
     for (var key in obj) {
-        // 如果是对象或数组，继续往深处挖
         if (typeof obj[key] === 'object' && obj[key] !== null) {
-            deepClean(obj[key]);
+            nukeUpdate(obj[key]); // 递归挖掘下一层
         } else {
-            // === 核心打击列表 ===
+            // 把键名转成小写，方便匹配
+            var lowKey = key.toLowerCase();
             
-            // 1. 修改版本号 (服务器版本改为0)
-            if (key === "appVersion" || key === "version_code" || key === "serverVersion") {
-                obj[key] = 0;
-            }
-            
-            // 2. 修改提示阈值
-            if (key === "promptMaxVersionCode" || key === "allowMinVersionCode") {
-                obj[key] = "0";
-            }
-            
-            // 3. 修改展示版本
-            if (key === "version" || key === "versionName") {
-                obj[key] = "1.0.0";
-            }
-            
-            // 4. 去除强制标记 (常见字段 force, mustUpdate, needUpdate)
-            if (key === "force" || key === "mustUpdate" || key === "needUpdate" || key === "isUpdate") {
-                obj[key] = 0; // 或者 false
-            }
-            
-            // 5. 破坏更新链接
-            if (key === "appUpdateURL" || key === "downloadUrl" || key === "url") {
-                // 只清空看起来像更新包的链接
-                if (typeof obj[key] === 'string' && (obj[key].indexOf(".apk") !== -1 || obj[key].indexOf("itunes") !== -1)) {
-                    obj[key] = "";
+            // 1. 任何包含 'version' 的字段 -> 改为本地版本号 '1.0.0' 或 0
+            if (lowKey.indexOf('version') !== -1) {
+                // 如果原值是数字，改 0；如果是字符串，改 '1.0.0'
+                if (typeof obj[key] === 'number') {
+                    obj[key] = 99999; // 改成超级大，骗它是未来版本
+                } else {
+                    obj[key] = "99.9.9"; // 改成超级大
                 }
+            }
+            
+            // 2. 任何包含 'update' 的字段 -> 改为空或 false
+            if (lowKey.indexOf('update') !== -1) {
+                if (typeof obj[key] === 'boolean') obj[key] = false;
+                else obj[key] = "";
+            }
+
+            // 3. 特殊关键词打击
+            if (lowKey === 'force' || lowKey === 'must') {
+                obj[key] = 0;
             }
         }
     }
@@ -65,18 +58,9 @@ function deepClean(obj) {
 if (body) {
     try {
         var obj = JSON.parse(body);
-        
-        // 开始全盘清洗
-        deepClean(obj);
-        
-        // 专门针对登录接口可能的特殊结构（如果它把数据放在 data 字符串里）
-        if (url.indexOf("getLoginMember") !== -1) {
-            console.log("正在处理福田登录接口...");
-        }
-
+        nukeUpdate(obj);
         $done({body: JSON.stringify(obj)});
     } catch (e) {
-        console.log("福田脚本执行错误: " + e);
         $done({});
     }
 } else {
